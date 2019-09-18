b2f00f86fca06cd98ef5ee4a6e7d2dfc0a023403
AlexandraBezbochina <55325098+AlexandraBezbochina@users.noreply.github.com>
2019-09-19 01:07:53 +0300

    добавление файлов

---
+++ homework_1(1).py
@@ -0,0 +1,24 @@
+print("homework from Bezbochina")
+print("парковка")
+def parkovka(day,hour):
+    if day%2==0:
+        print("четный день")
+        if hour>21:
+            print ("на правой стороне нельзя, паркуйся на левой")
+        elif 19<hour<21:
+            print("паркуйся, где хочешь")
+        else:
+            print("на левой стороне нельзя, паркуйся на правой")
+    elif day%2>0:
+        print("нечетный день")
+        if hour > 21:
+            print("на левой стороне нельзя, паркуйся на правой")
+        elif 19 < hour < 21:
+            print("паркуйся, где хочешь")
+        else:
+            print("на правой стороне нельзя, паркуйся на левой")
+
+parkovka(3,17.45)
+day=int(input("введите день"))
+hour=float(input("введите время в формате час.минуты "))
+parkovka(day,hour)
---
+++ homework_1(2).py
@@ -0,0 +1,13 @@
+print("homework from Bezbochina")
+print("палиндром")
+n=330
+n=str(n)
+if len(n)<4:
+    n=str(0)+n
+n
+
+if int(n[0])==int(n[3]):
+    if int(n[1])==int(n[2]):
+        print("является палиндромом")
+    else:
+        print("не является палиндромом")
